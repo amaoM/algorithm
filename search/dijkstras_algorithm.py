d1bb28a3f3828d447eba500339edca7db7fdeeb1
masashi0127 <chapa0106@gmail.com>
2016-10-18 19:49:05 +0900

    add

---
+++ search/dijkstras_algorithm.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+def dijkstras_algorithm(graph, start, goal):
+    cost = [100 for _ in range(len(graph))]
+    prev = [None for _ in range(len(graph))]
+    visited = [False for _ in range(len(graph))]
+
+    cost[start] = 0
+    prev[start] = start
+    node = start
+    while True:
+        min_cost = 100
+        visited[node] = True
+        next_node = -1
+        for i in range(len(graph)):
+            if visited[i] == True: continue
+            if graph[node][i]:
+                d = cost[node] + graph[node][i]
+                if cost[i] > d:
+                    cost[i] = d
+                    prev[i] = node
+            if min_cost > cost[i]:
+                min_cost = cost[i]
+                next_node = i
+        node = next_node
+        if next_node == -1: break
+
+    path = [goal]
+    while goal > start:
+        goal = prev[goal]
+        path.append(goal)
+
+    return sorted(path)
---
+++ search/tests/test_dijkstras_algorithm.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+import unittest
+import dijkstras_algorithm
+import random
+
+class TestDijkstrasAlgorithm(unittest.TestCase):
+
+    def test_dijkstras_algorithm_1(self):
+        graph = [
+            #s,   1, 2,  3,  4, t
+            [0,   6, 8, 18,  0, 0], #s
+            [6,   0, 0,  0, 11, 0], #1
+            [8,   0, 0,  9,  0, 7], #2
+            [18,  0, 9,  0,  0, 4], #3
+            [0,  11, 0,  0,  0, 3], #4
+            [0,   0, 7,  4,  3, 0], #t
+        ]
+        exp = [0, 2, 5]
+        start = 0
+        goal = 5
+        self.assertEqual(dijkstras_algorithm.dijkstras_algorithm(graph, start, goal), exp)
+
+    def test_dijkstras_algorithm_2(self):
+        graph = [
+            #s, 1, 2, 3, 4, t
+            [0, 5, 4, 2, 0, 0], #s
+            [5, 0, 2, 0, 0, 6], #1
+            [4, 2, 0, 3, 2, 0], #2
+            [2, 0, 3, 0, 6, 0], #3
+            [0, 0, 2, 6, 0, 4], #4
+            [0, 6, 0, 0, 4, 0], #t
+        ]
+        exp = [0, 2, 4, 5]
+        start = 0
+        goal = 5
+        self.assertEqual(dijkstras_algorithm.dijkstras_algorithm(graph, start, goal), exp)
